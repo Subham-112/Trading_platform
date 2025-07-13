6f2aa5127d8564aa33cf40935eb812313f12306c
Subham Sahu <subhambusiness286@gmail.com>
2025-07-13 19:25:59 +0530

    Signup Hero section Done

--- frontend/src/Landing_Page/Signup/signupHero.js
+++ frontend/src/Landing_Page/Signup/signupHero.js
@@ -1,5 +1,15 @@
 export default function SignupHero() {
   return (
-    <h1>Signup Hero</h1>
+    <div className="container mt-5 mb-5 pt-5">
+      <div className="row text-center">
+        <h1 className="mb-4" style={{ fontSize: "3rem", letterSpacing: '0.5px' }}>
+          Open a free demat and trading account online
+        </h1>
+        <h4 className="mt-1 text-muted" style={{ fontSize: '1.42rem',fontWeight: 400 }}>
+          Start investing brokerage free and join a community of 1.6+ crore
+          investors and traders
+        </h4>
+      </div>
+    </div>
   );
 }
