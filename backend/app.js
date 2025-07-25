d23acf24d11d3e76f667fbf8597d81867758ba1b
Subham Sahu <subhambusiness286@gmail.com>
2025-07-25 19:04:24 +0530

    Authentication complete

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
-
 const { PositionsModel } = require("./model/PositionsModel");
 const { OrdersModel } = require("./model/OrdersModel");
+const { UserModel: User } = require("./model/UserModel");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
@@ -17,6 +19,36 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const existUser = await User.findOne({ email });
+    if (existUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const newUser = new User({ email, password: hashedPass });
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.status(201).json({
+      message: "Signup Successful",
+      success: true,
+      token,
+      user: {
+        id: savedUser._id,
+        email: savedUser.email,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 app.get("/allHoldings", async (req, res) => {
   let allHoldings = await HoldingsModel.find({});
@@ -28,134 +60,134 @@ app.get("/allPositions", async (req, res) => {
   res.json(allPositions);
 });
 
-app.get("/postHoldings", async (req, res) => {
-  const Holdings = [
-    {
-      name: "BHARTIARTL",
-      qty: 2,
-      avg: 538.05,
-      price: 541.15,
-      net: "+0.58%",
-      day: "+2.99%",
-    },
-    {
-      name: "HDFCBANK",
-      qty: 2,
-      avg: 1383.4,
-      price: 1522.35,
-      net: "+10.04%",
-      day: "+0.11%",
-    },
-    {
-      name: "HINDUNILVR",
-      qty: 1,
-      avg: 2335.85,
-      price: 2417.4,
-      net: "+3.49%",
-      day: "+0.21%",
-    },
-    {
-      name: "INFY",
-      qty: 1,
-      avg: 1350.5,
-      price: 1555.45,
-      net: "+15.18%",
-      day: "-1.60%",
-      isLoss: true,
-    },
-    {
-      name: "ITC",
-      qty: 5,
-      avg: 202.0,
-      price: 207.9,
-      net: "+2.92%",
-      day: "+0.80%",
-    },
-    {
-      name: "KPITTECH",
-      qty: 5,
-      avg: 250.3,
-      price: 266.45,
-      net: "+6.45%",
-      day: "+3.54%",
-    },
-    {
-      name: "M&M",
-      qty: 2,
-      avg: 809.9,
-      price: 779.8,
-      net: "-3.72%",
-      day: "-0.01%",
-      isLoss: true,
-    },
-    {
-      name: "RELIANCE",
-      qty: 1,
-      avg: 2193.7,
-      price: 2112.4,
-      net: "-3.71%",
-      day: "+1.44%",
-    },
-    {
-      name: "SBIN",
-      qty: 4,
-      avg: 324.35,
-      price: 430.2,
-      net: "+32.63%",
-      day: "-0.34%",
-      isLoss: true,
-    },
-    {
-      name: "SGBMAY29",
-      qty: 2,
-      avg: 4727.0,
-      price: 4719.0,
-      net: "-0.17%",
-      day: "+0.15%",
-    },
-    {
-      name: "TATAPOWER",
-      qty: 5,
-      avg: 104.2,
-      price: 124.15,
-      net: "+19.15%",
-      day: "-0.24%",
-      isLoss: true,
-    },
-    {
-      name: "TCS",
-      qty: 1,
-      avg: 3041.7,
-      price: 3194.8,
-      net: "+5.03%",
-      day: "-0.25%",
-      isLoss: true,
-    },
-    {
-      name: "WIPRO",
-      qty: 4,
-      avg: 489.3,
-      price: 577.75,
-      net: "+18.08%",
-      day: "+0.32%",
-    }
-  ];
+// app.get("/postHoldings", async (req, res) => {
+//   const Holdings = [
+//     {
+//       name: "BHARTIARTL",
+//       qty: 2,
+//       avg: 538.05,
+//       price: 541.15,
+//       net: "+0.58%",
+//       day: "+2.99%",
+//     },
+//     {
+//       name: "HDFCBANK",
+//       qty: 2,
+//       avg: 1383.4,
+//       price: 1522.35,
+//       net: "+10.04%",
+//       day: "+0.11%",
+//     },
+//     {
+//       name: "HINDUNILVR",
+//       qty: 1,
+//       avg: 2335.85,
+//       price: 2417.4,
+//       net: "+3.49%",
+//       day: "+0.21%",
+//     },
+//     {
+//       name: "INFY",
+//       qty: 1,
+//       avg: 1350.5,
+//       price: 1555.45,
+//       net: "+15.18%",
+//       day: "-1.60%",
+//       isLoss: true,
+//     },
+//     {
+//       name: "ITC",
+//       qty: 5,
+//       avg: 202.0,
+//       price: 207.9,
+//       net: "+2.92%",
+//       day: "+0.80%",
+//     },
+//     {
+//       name: "KPITTECH",
+//       qty: 5,
+//       avg: 250.3,
+//       price: 266.45,
+//       net: "+6.45%",
+//       day: "+3.54%",
+//     },
+//     {
+//       name: "M&M",
+//       qty: 2,
+//       avg: 809.9,
+//       price: 779.8,
+//       net: "-3.72%",
+//       day: "-0.01%",
+//       isLoss: true,
+//     },
+//     {
+//       name: "RELIANCE",
+//       qty: 1,
+//       avg: 2193.7,
+//       price: 2112.4,
+//       net: "-3.71%",
+//       day: "+1.44%",
+//     },
+//     {
+//       name: "SBIN",
+//       qty: 4,
+//       avg: 324.35,
+//       price: 430.2,
+//       net: "+32.63%",
+//       day: "-0.34%",
+//       isLoss: true,
+//     },
+//     {
+//       name: "SGBMAY29",
+//       qty: 2,
+//       avg: 4727.0,
+//       price: 4719.0,
+//       net: "-0.17%",
+//       day: "+0.15%",
+//     },
+//     {
+//       name: "TATAPOWER",
+//       qty: 5,
+//       avg: 104.2,
+//       price: 124.15,
+//       net: "+19.15%",
+//       day: "-0.24%",
+//       isLoss: true,
+//     },
+//     {
+//       name: "TCS",
+//       qty: 1,
+//       avg: 3041.7,
+//       price: 3194.8,
+//       net: "+5.03%",
+//       day: "-0.25%",
+//       isLoss: true,
+//     },
+//     {
+//       name: "WIPRO",
+//       qty: 4,
+//       avg: 489.3,
+//       price: 577.75,
+//       net: "+18.08%",
+//       day: "+0.32%",
+//     }
+//   ];
 
-  Holdings.forEach((items) => {
-    let newHolding = new HoldingsModel({
-      name: items.name,
-      qty: items.qty,
-      avg: items.avg,
-      price: items.price,
-      net: items.net,
-      day: items.day
-    });
+//   Holdings.forEach((items) => {
+//     let newHolding = new HoldingsModel({
+//       name: items.name,
+//       qty: items.qty,
+//       avg: items.avg,
+//       price: items.price,
+//       net: items.net,
+//       day: items.day
+//     });
 
-    newHolding.save();
-  });
+//     newHolding.save();
+//   });
 
-  res.send('Done!');
-});
+//   res.send('Done!');
+// });
 
 // app.get('/postHoldings', async(req, res) => {
 //   const position = [{
@@ -211,7 +243,7 @@ app.post("/newOrder", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("App started!");
+  console.log("App started!", PORT);
   mongoose.connect(uri);
   console.log("DB started!");
 });
--- frontend/src/Landing_Page/Signup/signupSection.js
+++ frontend/src/Landing_Page/Signup/signupSection.js
@@ -1,4 +1,38 @@
+import { useState } from "react";
+
 export default function SignupSec() {
+  let [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3002/signup", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        }, body: JSON.stringify(user)
+      });
+
+      const data = await response.json();
+      const { message, success, email } = data;
+
+      if(success) {
+        localStorage.setItem('Email', email);
+        console.log(message);
+      }
+    } catch (err) {
+      console.error({ err: err.message })
+    }
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -35,9 +69,11 @@ export default function SignupSec() {
               </label>
               <input
                 className="w-75 fs-5 mb-3 align-self-center text-center border border-black rounded"
+                type="email"
                 name="email"
                 placeholder="Enter your Email"
                 style={{ height: "45px" }}
+                onChange={handleChange}
               />
 
               <label
@@ -49,18 +85,35 @@ export default function SignupSec() {
               </label>
               <input
                 className="w-75 fs-5 mb-3 align-self-center text-center border border-black rounded"
+                type="password"
                 name="password"
                 placeholder="Enter your Password"
                 style={{ height: "45px" }}
+                onChange={handleChange}
               />
 
               <span className="d-flex justify-content-center">
-                <button className="btn btn-primary w-25 fs-4">Signup</button>
+                <button
+                  className="btn btn-primary w-25 fs-4"
+                  type="submit"
+                  onClick={(e) => handleSubmit(e)}
+                >
+                  Signup
+                </button>
               </span>
             </form>
-            <p className="mt-4 text-muted" style={{ fontSize: '0.85rem',marginLeft: "5.2rem" }}>
-              By proceeding, you agree to the Zerodha <a style={{textDecoration: 'none'}} href="#">terms</a> &{" "}
-              <a style={{textDecoration: 'none'}} href="#">privacy policy</a>
+            <p
+              className="mt-4 text-muted"
+              style={{ fontSize: "0.85rem", marginLeft: "5.2rem" }}
+            >
+              By proceeding, you agree to the Zerodha{" "}
+              <a style={{ textDecoration: "none" }} href="#">
+                terms
+              </a>{" "}
+              &{" "}
+              <a style={{ textDecoration: "none" }} href="#">
+                privacy policy
+              </a>
             </p>
           </div>
         </div>
