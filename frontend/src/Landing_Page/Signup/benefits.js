b81c0a190121e56cb0476839e477cbde03e779ce
Subham Sahu <subhambusiness286@gmail.com>
2025-07-25 15:40:15 +0530

    Signup Benefit component complete

--- frontend/src/Landing_Page/Signup/benefits.js
+++ frontend/src/Landing_Page/Signup/benefits.js
@@ -1,5 +1,48 @@
 export default function Benefits() {
-    return (
-        <h1>Benefits</h1>
-    )
+  return (
+    <div className="container" style={{ marginTop: "7rem" }}>
+      <div className="row">
+        <div className="col ps-5">
+          <span className="h-100 d-flex flex-column justify-content-center align-items-center gap-5">
+            <img
+              width="350px"
+              src="/media/images/acop-benefits.svg"
+              alt="Benefit"
+            />
+            <h3>Benefits of opening a Zerodha demat account</h3>
+          </span>
+        </div>
+        <div className="col">
+          <span style={{ display: "block", marginBottom: "3rem" }}>
+            <h4 style={{ marginBottom: "1.5rem" }}>Unbeatable pricing</h4>
+            <p className="text-muted" style={{ fontSize: "1.1rem", width: '80%' }}>
+              Zero charges for equity & mutual fund investments. Flat ₹20 fees
+              for intraday and F&O trades.
+            </p>
+          </span>
+
+          <span style={{ display: "block", marginBottom: "3rem" }}>
+            <h4 style={{ marginBottom: "1.5rem" }}>Best investing experience</h4>
+            <p className="text-muted" style={{ fontSize: "1.1rem", width: '80%' }}>
+              Simple and intuitive trading platform with an easy-to-understand user interface.
+            </p>
+          </span>
+
+          <span style={{ display: "block", marginBottom: "3rem" }}>
+            <h4 style={{ marginBottom: "1.5rem" }}>No spam or gimmicks</h4>
+            <p className="text-muted" style={{ fontSize: "1.1rem", width: '80%' }}>
+              Committed to transparency — no gimmicks, spam, "gamification", or intrusive push notifications.
+            </p>
+          </span>
+
+          <span style={{ display: "block", marginBottom: "3rem" }}>
+            <h4 style={{ marginBottom: "1.5rem" }}>The Zerodha universe</h4>
+            <p className="text-muted" style={{ fontSize: "1.1rem", width: '80%' }}>
+              More than just an app — gain free access to the entire ecosystem of our partner products.
+            </p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
